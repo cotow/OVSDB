7b9af0ab1726208ba852f3da821de72d965f217d
Wagner Coto Meneses <wcoto@hpe.com>
2017-06-23 14:41:04 -0600

    Obtiene el url extra agregado en el cliente

--- initial.js
+++ initial.js
@@ -3,8 +3,13 @@
  */
 
 var http = require('http');
+const port = 8080;
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end("Provecho en el almuerzo Wagner");
-}).listen(8080);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(req.url);
+    res.end();
+    // res.end("My first server...");
+}).listen(port);
+
+console.log('Server is listening on http://localhost:'+port+'/');
