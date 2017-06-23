a79335d0cb034bb4edc76c406e750f76929e3388
Wagner Coto Meneses <wcoto@hpe.com>
2017-06-23 14:45:04 -0600

    Realiza un parseo de los datos contenidos en el url

--- initial.js
+++ initial.js
@@ -3,12 +3,15 @@
  */
 
 var http = require('http');
+var url  = require('url');
+
 const port = 8080;
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(req.url);
-    res.end();
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.end(txt);
     // res.end("My first server...");
 }).listen(port);
 
